a6eb2c0e8e48a881e173b2556431f108ff36ad5b
vrafexzo <113569103+vrafexzo@users.noreply.github.com>
2023-01-02 13:46:30 +0700

    tugas ucapan Vico Rafelino 2272011

---
+++ 2272011_Vico Rafelino_B/2272011_Vico Rafelino_B/index.js
@@ -0,0 +1,9 @@
+// 2272011_Vico Rafelino
+
+function myFunction() {
+    document.getElementById("judul").innerHTML="<b>Liburan telah usai nihh</b>";
+    document.body.style.backgroundImage = "url('img/wallpaper123.png')";
+    document.getElementById("p1").innerHTML="<b>Yeyyyy Besok Kita Masuk Kuliah Lagi >_< </b>";
+    document.getElementById("p2").innerHTML="<b>Saatnya kita kembali ke kampus. Perubahan rutinitas ini tentu akan membuat kita jadi moody dan kurang bersemangat. Yang tadinya rebahan mulu , bisa tidur larut malam dan lupa klo ada fitur bangun, besok kita kembali harus bangun pagi untuk berangkat ke kampus.  </b>";
+    document.getElementById("btm").innerHTML="<b>Semangat buat kuliah besok!!!</b>";
+}
